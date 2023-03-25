56bbf5d655407e9c6702d4cc54fd5d0d0d0b8509
Anastasiy1307 <100847962+Anastasiy1307@users.noreply.github.com>
2023-03-25 20:15:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diplom/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.diplom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int DELAY = 1500;
+    int defTimeOut = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( MainActivity.this, LogIn.class);
+                startActivity(intent);
+            }
+        }, DELAY);
+}
+}
