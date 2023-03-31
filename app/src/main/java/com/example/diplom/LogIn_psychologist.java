5cf8155c91bb865d2a6a04a24309a59aa07a57d4
Anastasiy1307 <100847962+Anastasiy1307@users.noreply.github.com>
2023-03-31 23:23:11 +0300

    Log

--- app/src/main/java/com/example/diplom/LogIn_psychologist.java
+++ app/src/main/java/com/example/diplom/LogIn_psychologist.java
@@ -2,14 +2,42 @@ package com.example.diplom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogIn_psychologist extends AppCompatActivity {
+
+    EditText login, password;
+    Button Login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_psychologist);
+        login = (EditText) findViewById(R.id.LogIn);
+        password = (EditText) findViewById(R.id.password);
+        Login = (Button) findViewById(R.id.Login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(login.getText().toString().equals("123") && password.getText().toString().equals("123")){
+                    Toast.makeText(LogIn_psychologist.this, "Вход выполнен успешно", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LogIn_psychologist.this, Glavn_Ps.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(LogIn_psychologist.this, "Проверьте логин или пароль", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/diplom/LogIn_student.java
+++ app/src/main/java/com/example/diplom/LogIn_student.java
@@ -2,13 +2,106 @@ package com.example.diplom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class LogIn_student extends AppCompatActivity {
+
+    EditText group;
+    Button LogIn;
+
+    Connection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_student);
+        group = (EditText) findViewById(R.id.Group);
+        LogIn = (Button) findViewById(R.id.Login);
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new LogIn_student.LogInStudent().execute("");
+
+            }
+        });
+    }
+
+    public class LogInStudent extends AsyncTask<String, String, String> {
+
+        String z = null;
+        Boolean isSuccess = false;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            ConnectionHelper connectionHelper = new ConnectionHelper();
+            connection = connectionHelper.connectionClass();
+            if (connection == null) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LogIn_student.this, "Проверьте подключение к Интернету", Toast.LENGTH_LONG).show();
+                    }
+                });
+                z = "On Internet Connection";
+            }
+            else {
+                try {
+                    String sql = "Select Name_group From Classes Where Name_group = '" + group.getText() + "'";
+                    Statement statement = connection.createStatement();
+                    ResultSet rs = statement.executeQuery(sql);
+
+                    if (rs.next()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LogIn_student.this, "Вход выполнен успешно", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        z = "Успешно";
+
+                        Intent intent = new Intent(LogIn_student.this, Tests.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LogIn_student.this, "Группа не найдена", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                    }
+                } catch (Exception e) {
+                    isSuccess = false;
+                    Log.e("SQL Error: ", e.getMessage());
+                }
+            }
+            return z;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
     }
 }
+
+
