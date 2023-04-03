bb57664ba8d156ba93bdd020f03e5efaa18c2b7d
Anastasiy1307 <100847962+Anastasiy1307@users.noreply.github.com>
2023-04-03 14:10:47 +0300

    Выбор корпуса

--- app/src/main/java/com/example/diplom/Korpus.java
+++ app/src/main/java/com/example/diplom/Korpus.java
@@ -1,14 +1,92 @@
 package com.example.diplom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.sql.Connection;
+import java.util.Base64;
 
 public class Korpus extends AppCompatActivity {
 
+    ImageView korpus;
+    Buildings mask;
+    Connection connection;
+    View v;
+    String img="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_korpus);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mask=getIntent().getParcelableExtra("korpus");
+
+        korpus=findViewById(R.id.korpus_img);
+
+        korpus.setImageBitmap(getImgBitmap(mask.getImg()));
+        v =findViewById(com.google.android.material.R.id.ghost_view);
+    }
+    private Bitmap getImgBitmap(String encodedImg) {
+        if(encodedImg!=null&& !encodedImg.equals("null")) {
+            byte[] bytes = new byte[0];
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                bytes = Base64.getDecoder().decode(encodedImg);
+            }
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        }
+        return BitmapFactory.decodeResource(Korpus.this.getResources(),
+                R.drawable.nophoto);
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && data!= null && data.getData()!= null)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Log.d("MyLog","Image URI : "+data.getData());
+                korpus.setImageURI(data.getData());
+                Bitmap bitmap = ((BitmapDrawable)korpus.getDrawable()).getBitmap();
+                encodeImage(bitmap);
+
+            }
+        }
+    }
+
+    private void getImage()
+    {
+        Intent intentChooser= new Intent();
+        intentChooser.setType("image/*");
+        intentChooser.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentChooser,1);
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        int prevW = 150;
+        int prevH = bitmap.getHeight() * prevW / bitmap.getWidth();
+        Bitmap b = Bitmap.createScaledBitmap(bitmap, prevW, prevH, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            img=Base64.getEncoder().encodeToString(bytes);
+            return img;
+        }
+        return "";
     }
 }
