bf4320e03848b24ab52b86a7717ad2db096c9875
Anastasiy1307 <100847962+Anastasiy1307@users.noreply.github.com>
2023-05-12 19:29:31 +0300

    Тесты готовы

--- app/src/main/java/com/example/diplom/Questions3.java
+++ app/src/main/java/com/example/diplom/Questions3.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -20,65 +22,277 @@ public class Questions3 extends AppCompatActivity {
     TextView nameTest;
     View v;
     Connection connection;
-    List<masks_Questions3> data;
-    ListView listView;
-    Adapter_Questions3 pAdapter;
+    String ConnectionResult = "";
+    String test = "";
+    public final int[] i = {0};
+
+    Button yes;
+    Button ts;
+    Button not;
+
+
+    float ball = 0;
+    float ball11 = 0;
+    float ball22 = 0;
+    float ball33 = 0;
+    float ball44 = 0;
+    int balll1;
+    int ball1 = 0;
+    int balll2;
+    int ball2 = 0;
+    int balll3;
+    int ball3 = 0;
+    int balll4;
+    int ball4 = 0;
+
+    String ID;
+
+    TextView bal;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions3);
         v = findViewById(com.google.android.material.R.id.ghost_view);
-
-        nameTest = (TextView) findViewById(R.id.nameTestt);
+        nameTest = (TextView) findViewById(R.id.nameTest);
         final String getExtra = getIntent().getStringExtra("test");
-
+        final String getExtra1= getIntent().getStringExtra("ID");
+        test = getExtra;
+        ID = getExtra1;
         nameTest.setText(getExtra);
+        GetTextFromSql1(v);
 
-        GetTextFromSQL(v);
+
+
+
+
     }
+
+
+
 
 
     public void End(View v) {
+
+        try {
+            ConnectionHelper conectionHellper = new ConnectionHelper();
+            connection = conectionHellper.connectionClass();
+            ball = ball1+ ball2+ball3+ball4;
+            if (ball < 0 )
+            {
+                Toast.makeText(this,"Тест еще не закончен", Toast.LENGTH_LONG).show();
+                return;
+            }
+            if (connection != null) {
+                ball = ball/16;
+                String query = "Update Result set Result = '"+ball+"' Where ID = '"+ID+"' and Result is NULL ";
+                Statement statement = connection.createStatement();
+                statement.execute(query);
+                Toast.makeText(this,"Успешно добавлено", Toast.LENGTH_LONG).show();
+            } else {
+                ConnectionResult = "Check Connection";
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        ball11 =ball1/4;
+        ball22 =ball2/4;
+        ball33 =ball3/4;
+        ball44 =ball4/4;
+
         Intent intent = new Intent(Questions3.this, ResultTests.class);
+
+        intent.putExtra("балл Самооценка уровня творческой активности воспитанников",ball);
+        intent.putExtra("test",test);
+
+        intent.putExtra("чувство новизны",ball11);
+        intent.putExtra("критичность",ball22);
+        intent.putExtra("способность преобразовывать структуру объекта",ball33);
+        intent.putExtra("направленность на творчество",ball44);
+
         startActivity(intent);
         finish();
-    }
-    public void enterMobile() {
-        pAdapter.notifyDataSetInvalidated();
-        listView.setAdapter(pAdapter);
+
     }
 
-    public void GetTextFromSQL(View v) {
-        data = new ArrayList<masks_Questions3>();
-        listView = findViewById(R.id.Ql);
-        pAdapter = new Adapter_Questions3(Questions3.this, data);
+    public void GetTextFromSql1(View v) {
+        TextView BaseId = findViewById(R.id.Question);
+
         try {
-            ConnectionHelper connectionHelper = new ConnectionHelper();
-            connection = connectionHelper.connectionClass();
+            ConnectionHelper conectionHellper = new ConnectionHelper();
+            connection = conectionHellper.connectionClass();
             if (connection != null) {
 
-                String query = "Select * From Questions_t3";
+                String query0 = "select count(ID) from Questions_t3 ";
+                Statement statement0 = connection.createStatement();
+                ResultSet resultSet0 = statement0.executeQuery(query0);
+                int c = 0;
+                while (resultSet0.next()) {
+                    c = resultSet0.getInt(1);
+                }
+
+                int finalC = c;
+
+                if (i[0] != finalC) {
+                    i[0] = i[0] + 1;
+                    if (i[0] ==1 || i[0] == 5 || i[0] == 9 || i[0] == 13){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll1 = ball1 + 0;
+                                ball1 = balll1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll1 = ball1 + 2;
+                                ball1 = balll1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        ts = (Button) findViewById(R.id.ts);
+                        ts.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll1 = ball1 + 1;
+                                ball1 = balll1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+
+                    }
+                    else if (i[0] == 2 || i[0] == 6 || i[0] == 10 || i[0] == 14){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll2 = ball2 + 0;
+                                ball2 = balll2;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll2 = ball2 + 2;
+                                ball2 = balll2;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        ts = (Button) findViewById(R.id.ts);
+                        ts.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll2 = ball2 + 1;
+                                ball2 = balll2;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+
+                    }
+
+                    else if (i[0] == 3 || i[0] == 7 || i[0] == 11 || i[0] == 15){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll3 = ball3 + 0;
+                                ball3 = balll3;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll3 = ball3 + 2;
+                                ball3 = balll3;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        ts = (Button) findViewById(R.id.ts);
+                        ts.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll3 = ball3 + 1;
+                                ball3 = balll3;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                    }
+
+                    else if (i[0] == 4 || i[0] == 8 || i[0] == 12 || i[0] == 16){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll4 = ball4 + 0;
+                                ball4 = balll4;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll4 = ball4 + 2;
+                                ball4 = balll4;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        ts = (Button) findViewById(R.id.ts);
+                        ts.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                balll4 = ball4 + 1;
+                                ball4 = balll4;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+
+                    }
+                }else
+                {
+                    not.setEnabled(false);
+                    yes.setEnabled(false);
+                    ts.setEnabled(false);
+
+                }
+
+
+                String query = "Select * From Questions_t3 where ID = " + i[0] + "";
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
-
                 while (resultSet.next()) {
-                    masks_Questions3 tempMask = new masks_Questions3
-                            (resultSet.getInt("ID"),
-                                    resultSet.getString("Question")
-
-
-                            );
-                    data.add(tempMask);
-                    pAdapter.notifyDataSetInvalidated();
+                    BaseId.setText(resultSet.getString(2));
                 }
-                connection.close();
+
             } else {
+                ConnectionResult = "Check Connection";
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (Exception ex) {
+
         }
-        enterMobile();
 
     }
+
 }
--- app/src/main/java/com/example/diplom/Questions4.java
+++ app/src/main/java/com/example/diplom/Questions4.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -20,63 +22,219 @@ public class Questions4 extends AppCompatActivity {
     TextView nameTest;
     View v;
     Connection connection;
-    List<maska_questions4> data;
-    ListView listView;
-    Adapter_Questions4 pAdapter;
+    String ConnectionResult = "";
+    String test = "";
+    public final int[] i = {0};
+
+    Button yes;
+    Button not;
+    int ballks1;
+    int ballos1;
+    int ball;
+    int ballks = 0;
+    int ballos = 0;
+
+    String ID;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions4);
         v = findViewById(com.google.android.material.R.id.ghost_view);
-
-        nameTest = (TextView) findViewById(R.id.nameTests);
+        nameTest = (TextView) findViewById(R.id.nameTest);
         final String getExtra = getIntent().getStringExtra("test");
-
+        final String getExtra1= getIntent().getStringExtra("ID");
+        test = getExtra;
+        ID = getExtra1;
         nameTest.setText(getExtra);
+        GetTextFromSql1(v);
 
-        GetTextFromSQL(v);
+
+
+
+
     }
+
+
+
 
 
     public void End(View v) {
+
+        try {
+            ConnectionHelper conectionHellper = new ConnectionHelper();
+            connection = conectionHellper.connectionClass();
+            ball = ballks + ballos;
+            if (ball < 0 )
+            {
+                Toast.makeText(this,"Тест еще не закончен", Toast.LENGTH_LONG).show();
+                return;
+            }
+            if (connection != null) {
+                String query = "Update Result set Result = '"+ball+"' Where ID = '"+ID+"' and Result is NULL ";
+                Statement statement = connection.createStatement();
+                statement.execute(query);
+                Toast.makeText(this,"Успешно добавлено", Toast.LENGTH_LONG).show();
+            } else {
+                ConnectionResult = "Check Connection";
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+
         Intent intent = new Intent(Questions4.this, ResultTests.class);
+
+        intent.putExtra("КОС-2",ball);
+        intent.putExtra("test",test);
+
+        intent.putExtra("Коммуникативные склонности",ballks);
+        intent.putExtra("Организаторские склонности",ballos);
+
+
         startActivity(intent);
         finish();
-    }
-    public void enterMobile() {
-        pAdapter.notifyDataSetInvalidated();
-        listView.setAdapter(pAdapter);
+
     }
 
-    public void GetTextFromSQL(View v) {
-        data = new ArrayList<maska_questions4>();
-        listView = findViewById(R.id.Ql);
-        pAdapter = new Adapter_Questions4(Questions4.this, data);
+    public void GetTextFromSql1(View v) {
+        TextView BaseId = findViewById(R.id.Question);
+
         try {
-            ConnectionHelper connectionHelper = new ConnectionHelper();
-            connection = connectionHelper.connectionClass();
+            ConnectionHelper conectionHellper = new ConnectionHelper();
+            connection = conectionHellper.connectionClass();
             if (connection != null) {
 
-                String query = "Select * From Questions_t4";
+                String query0 = "select count(ID) from Questions_t4 ";
+                Statement statement0 = connection.createStatement();
+                ResultSet resultSet0 = statement0.executeQuery(query0);
+                int c = 0;
+                while (resultSet0.next()) {
+                    c = resultSet0.getInt(1);
+                }
+
+                int finalC = c;
+
+                if (i[0] != finalC) {
+                    i[0] = i[0] + 1;
+                    if (i[0] ==1 || i[0] == 5 || i[0] == 9 || i[0] == 13 || i[0] == 17){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballks1 = ballks + 0;
+                                ballks = ballks1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballks1 = ballks + 1;
+                                ballks = ballks1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+
+                    }
+                    else if (i[0] == 3 || i[0] == 7 || i[0] == 11 || i[0] == 15 || i[0] == 19){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballks1 = ballks + 1;
+                                ballks = ballks1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballks1 = ballks + 0;
+                                ballks = ballks1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+
+                    }
+
+                    else if (i[0] == 2 || i[0] == 6 || i[0] == 10 || i[0] == 14 || i[0] == 18){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballos1 = ballos + 0;
+                                ballos = ballos1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballos1 = ballos + 1;
+                                ballos = ballos1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                    }
+
+                    else if (i[0] == 4 || i[0] == 8 || i[0] == 12 || i[0] == 16 || i[0] == 20){
+                        not = (Button) findViewById(R.id.not);
+                        not.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballos1 = ballos + 1;
+                                ballos = ballos1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                        yes = (Button) findViewById(R.id.yes);
+                        yes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ballos1 = ballos + 0;
+                                ballos = ballos1;
+                                GetTextFromSql1(v);
+
+                            }
+                        });
+                    }
+                }else
+                {
+                    not.setEnabled(false);
+                    yes.setEnabled(false);
+
+
+                }
+
+
+                String query = "Select * From Questions_t4 where ID = " + i[0] + "";
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
-
                 while (resultSet.next()) {
-                    maska_questions4 tempMask = new maska_questions4
-                            (resultSet.getInt("ID"),
-                                    resultSet.getString("Question")
-                            );
-                    data.add(tempMask);
-                    pAdapter.notifyDataSetInvalidated();
+                    BaseId.setText(resultSet.getString(2));
                 }
-                connection.close();
+
             } else {
+                ConnectionResult = "Check Connection";
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (Exception ex) {
+
         }
-        enterMobile();
 
     }
+
 }
